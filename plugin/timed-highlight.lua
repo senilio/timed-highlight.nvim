7a5765732068ae488b9113748910f085e3824b16
senilio <git@melt.se>
2024-11-05 13:02:29 +0100

    Add search fix from https://github.com/sahlte/timed-highlight.nvim/issues/2

--- plugin/timed-highlight.lua
+++ plugin/timed-highlight.lua
@@ -1,28 +1,40 @@
 -- ensure n and N highlight for only a brief time
 
 local function searchAndOpenFold(key)
-    vim.cmd('normal! ' .. key)
+	-- Get the current search term
+	local search_term = vim.fn.getreg("/")
 
-    if string.find(vim.o.foldopen, "search") and vim.fn.foldclosed('.') ~= -1 then
-        vim.cmd('normal! zv')
-    end
+	-- Use pcall to safely execute the command and catch any errors
+	local success, _ = pcall(function()
+		vim.cmd("normal! " .. key)
+	end)
 
-    require('timed-highlight').turn_off_highlight_after_expiration()
+	if not success then
+		vim.api.nvim_err_writeln("E486: Pattern not found: " .. search_term)
+	end
+
+	if string.find(vim.o.foldopen, "search") and vim.fn.foldclosed(".") ~= -1 then
+		vim.cmd("normal! zv")
+	end
+
+	require("timed-highlight").turn_off_highlight_after_expiration()
 end
 
-vim.keymap.set('n', 'n', function() searchAndOpenFold('n') end, {noremap = true, silent = true})
-vim.keymap.set('n', 'N', function() searchAndOpenFold('N') end, {noremap = true, silent = true})
+vim.keymap.set("n", "n", function()
+	searchAndOpenFold("n")
+end, { noremap = true, silent = true })
+vim.keymap.set("n", "N", function()
+	searchAndOpenFold("N")
+end, { noremap = true, silent = true })
 
 -- ensure the initial lookup using / or ? highlight for only a brief time
 vim.api.nvim_create_autocmd("CmdlineLeave", {
-    callback = function()
-        local cmd_type = vim.fn.expand("<afile>")
-        vim.schedule(
-            function()
-                if cmd_type ~= nil and (cmd_type == '/' or cmd_type == '?') then
-                    require('timed-highlight').turn_off_highlight_after_expiration()
-                end
-            end
-        )
-    end
+	callback = function()
+		local cmd_type = vim.fn.expand("<afile>")
+		vim.schedule(function()
+			if cmd_type ~= nil and (cmd_type == "/" or cmd_type == "?") then
+				require("timed-highlight").turn_off_highlight_after_expiration()
+			end
+		end)
+	end,
 })
